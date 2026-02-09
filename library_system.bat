11e6a2c08ed73a40e1123d10eb17ecde1cdd4ad2
David178-blip <marcdavidpatibo5@gmail.com>
2026-02-10 06:37:29 +0800

    system bat

---
+++ library_system.bat
@@ -0,0 +1,10 @@
+@echo off
+echo Starting Library System...
+echo ----------------------------------------
+echo This will run:
+echo 1. PHP Artisan Serve
+echo 2. Node Server
+echo 3. Vite (npm run dev)
+echo ----------------------------------------
+npm run dev:all
+pause
